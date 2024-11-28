e35cc92502ec860ae6520d733be28d3eee9d5fda
libiao <578556500@qq.com>
2024-11-28 10:00:22 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // 你的 ESLint 规则
+  },
+};
